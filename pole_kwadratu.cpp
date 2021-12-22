dea8768a09087c5d186ad0ef63d7adf7c738ef41
Patryk <66219634+kyrtaP-tech@users.noreply.github.com>
2021-12-22 14:37:56 +0100

    Add files via upload

---
+++ pole_kwadratu.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, mnozenie;
+	cout << "Podaj dlugosc boku: ";
+	cin >> a;
+	mnozenie = a*a;
+	cout << "Pole kwadratu wynosi: " << mnozenie;
+    return 0;
+}
